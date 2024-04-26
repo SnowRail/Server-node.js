1b7e190fc8f712da4e5b997dde82690590ad6bad
정종문 <whdans4005@naver.com>
2024-04-26 22:29:07 +0900

    node로 변경

---
+++ server.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const server = net.createServer((socket) => {
+  console.log('클라이언트 연결됨: ' + socket.remoteAddress + ':' + socket.remotePort);
+
+  socket.on('data', (data) => {
+    console.log('수신 데이터: ' + data.toString());
+    socket.write('수신한 데이터를 다시 전송합니다: ' + data);
+  });
+
+  socket.on('end', () => {
+    console.log('클라이언트 연결 종료됨: ' + socket.remoteAddress + ':' + socket.remotePort);
+  });
+});
+
+server.listen(30303, () => {
+  console.log('서버 시작됨: ' + server.address().address + ':' + server.address().port);
+});
