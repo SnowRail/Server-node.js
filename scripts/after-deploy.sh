3d6a8361c77429f0fa85050f1f0da8e7d85dc749
정종문 <whdans4005@naver.com>
2024-04-27 15:46:25 +0900

    fuser

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,6 +4,6 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 sudo npm
-# sudo pm2 start dist
-# node server.js > /dev/null 2> /dev/null < /dev/null &
+fuser -k 30303/tcp
 node server.js > server.log 2>&1 &
+# sudo pm2 start dist
