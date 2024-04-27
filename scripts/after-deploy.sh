f55c91d953da8bb03f20a2985bf50dab279ffc1c
정종문 <whdans4005@naver.com>
2024-04-27 11:38:22 +0900

    plz

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,7 +3,7 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
-# sudo npm # dependency 패키지 설치
+sudo npm # dependency 패키지 설치
 
 # sudo pm2 start dist
-# node server.js > /dev/null 2> /dev/null < /dev/null & # 서버 재시작
+node ../server.js > /dev/null 2> /dev/null < /dev/null & # 서버 재시작
