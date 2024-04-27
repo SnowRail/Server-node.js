4e18cc239e69a97544cc6b010d16c59587108ba0
정종문 <whdans4005@naver.com>
2024-04-27 15:33:42 +0900

    서버 출력 로그 파일 저장

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,10 +2,8 @@
 REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
-touch after-deploy-log
-echo after-deploy.sh run >> after-deploy-log
-cat after-deploy-log
-sudo npm # dependency 패키지 설치
 
+sudo npm
 # sudo pm2 start dist
-node server.js > /dev/null 2> /dev/null < /dev/null & # 서버 재시작
+# node server.js > /dev/null 2> /dev/null < /dev/null &
+node server.js > server.log 2>&1 &
