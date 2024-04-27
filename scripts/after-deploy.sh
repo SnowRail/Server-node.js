8fdf1e766482e2e93feb3171162da343690be0fa
정종문 <whdans4005@naver.com>
2024-04-27 15:19:24 +0900

    node run in background

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -5,7 +5,7 @@ cd $REPOSITORY
 touch after-deploy-log
 echo after-deploy.sh run >> after-deploy-log
 cat after-deploy-log
-# sudo npm # dependency 패키지 설치
+sudo npm # dependency 패키지 설치
 
 # # sudo pm2 start dist
-# node ../server.js > /dev/null 2> /dev/null < /dev/null & # 서버 재시작
+node ../server.js > /dev/null 2> /dev/null < /dev/null & # 서버 재시작
