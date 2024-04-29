4a745088e335b15ff8f8a2c46a54aca36445e256
Erin <qwey1584@gmail.com>
2024-04-29 14:13:44 +0900

    Modify : removeObject

--- InGameServer/NetworkObjectManager.js
+++ InGameServer/NetworkObjectManager.js
@@ -13,7 +13,28 @@ function addObject(uInstance){
     m_PlayerLists.push(uInstance);
 }
 
+function removeObject(uInstance){
+    if(!(uInstance instanceof UnityInstance)){
+        throw new Error('Only Unity Instance can be removed.');
+    }
+    m_PlayerLists.splice(m_PlayerLists.indexOf(uInstance),1);
+}
+
+function removeObjectByID(clientID){
+    const newList = [];
+    for(let i = 0; i<m_PlayerLists.length; ++i){
+        const PlayerObject = m_PlayerLists[i];
+
+        if(PlayerObject.clientID !== clientID){
+            newList.push(PlayerObject);
+        }
+    }
+    m_PlayerLists = newList;
+}
+
 module.exports = {
     getObjects,
-    addObject
+    addObject,
+    removeObject,
+    removeObjectByID,
 };
