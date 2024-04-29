b94d3692bc727def588d2a4128a818684f3bee2e
Erin <qwey1584@gmail.com>
2024-04-29 20:36:29 +0900

    Add : protocol

--- InGameServer/Protocol.js
+++ InGameServer/Protocol.js
@@ -1,12 +1,14 @@
 const Protocol = {
     s_NewUser : 29,
-    s_PlayerConnect: 30,
-    c_PlayerCheckConn : 31,
-    c_PlayerPosition: 32,
-    s_PlayerPosition : 33,
+    PlayerMove : 30,
+    s_PlayerConnect : 31,
+    c_PlayerReconnect : 32,
 
-
-    s_DestroyPlayer : 58,
+    LoadGameScene : 50, // 게임 씬으로 전환
+    GameStartCountDown : 51, // 게임 시작 전 카운트 다운
+    GameStart : 52, // 게임 시작
+    GameEndCountDown : 53, // 1등 도착 후 카운트 다운 시작
+    GameEnd : 54, // 게임 종료
 };
 
 module.exports = Protocol;
