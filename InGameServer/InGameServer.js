ce3ef801379da3b8f2bc1c5ef01dfc28512ea6fb
Erin <qwey1584@gmail.com>
2024-04-30 15:13:41 +0900

    Add : ProtocolHandler - GameStart

--- InGameServer/InGameServer.js
+++ InGameServer/InGameServer.js
@@ -14,6 +14,7 @@ const {
     PlayerDisconnect,
     PlayerGoal,
     CountDown,
+    GameStart,
 } = require('./ProtocolHandler');
 
 const idList = [];
@@ -52,9 +53,11 @@ const server = net.createServer((socket) =>
                 const playerPos = byteReader.readVector3();
                 UpdatePlayerPos(socket, syncId, playerPos);
                 break;
+            case Protocol.GameStart:
+                GameStart(socket);
+                break;
             case Protocol.PlayerGoal:
                 const goalId = byteReader.readInt();   
-
                 PlayerGoal(socket, goalId);
                 break;
             case Protocol.GameEndCountDown:
