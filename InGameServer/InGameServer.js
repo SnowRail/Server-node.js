e93e90cbf9fc591138654e66e98eb5d1393e343f
Erin <qwey1584@gmail.com>
2024-04-30 15:03:34 +0900

    Modify : delete console.log

--- InGameServer/InGameServer.js
+++ InGameServer/InGameServer.js
@@ -6,6 +6,7 @@ const UnityInstance = require('./UnityClass/UnityInstance');
 const { intSize, floatSize } = require('./typeSize');
 const SocketManager = require('./SoketManager');
 
+
 const {
     FirstConn,
     UpdatePlayerPos,
@@ -44,17 +45,11 @@ const server = net.createServer((socket) =>
             case Protocol.PlayerMove:
                 const moveId = byteReader.readInt();
                 const playerDirection = byteReader.readVector3();
-
-                console.log("update dirc id: " , moveId, "pos : ", playerDirection);
-
                 UpdatePlayerDirection(socket, moveId, playerDirection);
                 break;
             case Protocol.SyncPosition:
                 const syncId = byteReader.readInt();
                 const playerPos = byteReader.readVector3();
-
-                console.log("update pos id: " , syncId, "pos : ", playerPos);
-
                 UpdatePlayerPos(socket, syncId, playerPos);
                 break;
             case Protocol.PlayerGoal:
