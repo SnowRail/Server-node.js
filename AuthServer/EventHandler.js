e0ae12552c9553dfddb3f665fef9462cedf290b5
Erin <qwey1584@gmail.com>
2024-05-07 23:07:12 +0900

    Modify : add dotenv

--- AuthServer/EventHandler.js
+++ AuthServer/EventHandler.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port : process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
