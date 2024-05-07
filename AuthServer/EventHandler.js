23283d56861424f0ad06f08e9665c3de902781ce
Kim-YeRim <1000gaji@gmail.com>
2024-05-07 22:15:24 +0900

    Modify : DB auth -> .sh

--- AuthServer/EventHandler.js
+++ AuthServer/EventHandler.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: process.env.SSH_HOST, //'outgame-database.cin32mhxbadg.ap-northeast-2.rds.amazonaws.com',
-    port : 3306,
-    user: 'admin',
-    password: process.env.SSH_PASSWORD, //'vibracoffee',
-    database: 'OutGameDB'
+    host: process.env.MYSQL_HOST,
+    port : process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 connection.connect((err) => {
