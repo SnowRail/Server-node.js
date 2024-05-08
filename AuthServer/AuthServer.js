2728e10c29081fcea17d91dbc60fc3ffc500c907
Kim-YeRim <1000gaji@gmail.com>
2024-05-08 15:26:33 +0900

    Modify : app -> server listen

--- AuthServer/AuthServer.js
+++ AuthServer/AuthServer.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const {Server} = require('socket.io');
+const {Server} = require("socket.io");
 const io = new Server(server);
 
 const { Login, Signup } = require('./EventHandler');
 
 io.on('connection', (socket) => {
-    console.log('a user connected : ', socket.remoteAddress + ":" + socket.remotePort);
+    //console.log('a user connected : ', socket.remoteAddress + ":" + socket.remotePort);
 
     socket.on('login', (msg) => {
         console.log('login : ', msg);
@@ -20,8 +20,7 @@ io.on('connection', (socket) => {
         console.log('signup : ', msg);
         Signup(socket, msg);
     });
-
-
+    console.log("연결됨 : " + socket.id);
 
     socket.on('disconnect', () => {
         console.error('user disconnected : ', socket.remoteAddress + ":" + socket.remotePort);
@@ -29,9 +28,7 @@ io.on('connection', (socket) => {
 });
 
 
-
-
-app.listen(10101, () => {    
+server.listen(10101, () => {    
     console.log('Example app listening on port 10101!');
 }).on('error', (err) => {
     console.error('Server error : ', err);
