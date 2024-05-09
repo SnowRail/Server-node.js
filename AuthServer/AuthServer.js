48939b772661408dd05d692b4a291eab53eaf585
Erin <qwey1584@gmail.com>
2024-05-09 22:13:03 +0900

    Modify : console -> logger

--- AuthServer/AuthServer.js
+++ AuthServer/AuthServer.js
@@ -4,41 +4,42 @@ const http = require('http');
 const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
+const logger = require('./logger');
 
 const {   
     Login, 
     Signup,
     MatchMaking 
 } = require('./EventHandler');
+const { log } = require('console');
 
 io.on('connection', (socket) => {
     //console.log('a user connected : ', socket.remoteAddress + ":" + socket.remotePort);
-    console.log("연결됨 : " + socket.id);
-
+    logger.info("연결됨 : " + socket.id);
     socket.on('login', (msg) => {
-        console.log('login : ', msg);
+        logger.info('login : ', msg);
         Login(socket, msg);
     });
 
     socket.on('signup', (msg) => {
-        console.log('signup : ', msg);
+        logger.info('signup : ', msg);
         Signup(socket, msg);
     });
     
     socket.on('matching', (msg) => {
-        console.log('matching : ', msg);
+        logger.info('matching : ', msg);
         MatchMaking(msg);
     });
 
     socket.on('disconnect', () => {
-        console.error('user disconnected : ', socket.remoteAddress + ":" + socket.remotePort);
+        logger.error('user disconnected : ', socket.remoteAddress + ":" + socket.remotePor);
         // TODO 접속한 플레이어 리스트에서 삭제하기
     });
 });
 
 
 server.listen(10101, () => {    
-    console.log('Example app listening on port 10101!');
+    logger.info('서버가 10101번 포트에서 실행 중입니다.');
 }).on('error', (err) => {
-    console.error('Server error : ', err);
+    logger.error('Server error : ', err);
 });
