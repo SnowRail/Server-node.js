a15975b8d11214bb9d860b44be7d7c1a120b302e
Erin <qwey1584@gmail.com>
2024-05-11 12:52:45 +0900

    Modify : debug

--- AuthServer/EventHandler.js
+++ AuthServer/EventHandler.js
@@ -50,38 +50,36 @@ function Login(socket, msg) {
             socket.emit('loginFail', 'first login fail');
             return;
         }
-        let queryResult;
         if (rows.length === 0) { // 등록되지 않은 사용자
             let newName;
             do {
                 newName = crypto.randomBytes(8).toString('hex');
             } while (setUniqueName(newName));
 
-            connection.query("INSERT INTO User (email, name) VALUES (?, ?)", [loginUser, newName], (err) => {
+            connection.query("INSERT INTO User (email, name) VALUES (?, ?)", [loginUser, newName], (err, result) => {
                 if (err) {
                     logger.error('Signup query error:', err);
                     socket.emit('signupFail', '회원 등록에 실패했습니다');
                     return;
                 }
-                else {
-                    connection.query('SELECT * FROM User WHERE email = ?', [loginUser], (err, member) => {
-                        if (err) {
-                            logger.error('Login query error:', err);
-                            socket.emit('loginFail', 'second login fail');
-                            return;
-                        }
-                        else
-                            queryResult = member[0];
-                    });
-                }
+                // INSERT 쿼리 실행 후 새로 등록된 사용자의 정보를 가져옵니다.
+                connection.query('SELECT * FROM User WHERE email = ?', [loginUser], (err, rows) => {
+                    if (err) {
+                        logger.error('Login query error:', err);
+                        socket.emit('loginFail', 'second login fail');
+                        return;
+                    }
+                    const queryResult = rows[0];
+                    socket.emit('inquiryPlayer', JSON.stringify(queryResult));
+                    connectedPlayers.set(loginUser, {socket : socket, room : null});
+                });
             });
         }
         else {
-            queryResult = rows[0];
+            const queryResult = rows[0];
+            socket.emit('inquiryPlayer', JSON.stringify(queryResult));
+            connectedPlayers.set(loginUser, {socket : socket, room : null});
         }
-        console.log("query : ", queryResult);
-        socket.emit('inquiryPlayer', JSON.stringify(queryResult));
-        connectedPlayers.set(loginUser, {socket : socket, room : null});
     });
 }
 
