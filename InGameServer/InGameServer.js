45757febbe1b5969524e24cf75a302dde233ef0b
Kim-YeRim <1000gaji@gmail.com>
2024-05-14 17:16:13 +0900

    Chore : debug log

--- InGameServer/InGameServer.js
+++ InGameServer/InGameServer.js
@@ -136,16 +136,16 @@ const outgameServer = net.createServer((socket) => {
 
     socket.on('data', (data) => {
         const msg = data.toString();
-        const receivedData = JSON.parse(msg);
-        const roomID = receivedData.roomID;
-        const playerList = receivedData.playerList;
-        playerList.forEach((player) => {
-            const socket = SocketManager.getSocketById(player.id);
-            if(socket)
-            {
-                // FirstConn(socket.player.id);
-            }
-        });
+        // const receivedData = JSON.parse(msg);
+        // const roomID = receivedData.roomID;
+        // const playerList = receivedData.playerList;
+        // playerList.forEach((player) => {
+        //     const socket = SocketManager.getSocketById(player.id);
+        //     if(socket)
+        //     {
+        //         // FirstConn(socket.player.id);
+        //     }
+        // });
         console.log('outgameServer : ', msg);
     });
 
