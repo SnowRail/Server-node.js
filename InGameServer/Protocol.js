5f47f5caa0c962bdf92ab0d419fd5bbe52c47792
Erin <qwey1584@gmail.com>
2024-05-16 22:04:00 +0900

    Modify : Protocol

--- InGameServer/Protocol.js
+++ InGameServer/Protocol.js
@@ -1,26 +1,13 @@
 const Protocol = {
-    Login : 1,                  // 로그인
-    Logout : 2,                 // 로그아웃
-    Signin : 3,                 // 회원가입
-
-    StartMatchMaking : 10,      // 매치메이킹 시작
-    EnterWaitingRoom : 11,      // 대기실 입장
-    LoadGameScene : 12,         // 인게임 씬으로 전환  - 멍청 레이싱에서는 playerconnect로 사용
-    PlayerReady : 13,           // 로딩 완료
-    GameStartCountDown : 14,         // 게임 시작 / 끝 카운트 다운
-    GameStart : 15,             // 게임 시작
-
-    Key : 20,                   // 키 입력
-    Item : 21,                  // 아이템 사용
-    PlayerReconnect : 22,       // 플레이어 재접속
-    PlayerDisconnect : 23,      // 플레이어 접속 끊김
-    PlayerGoal : 24,            // 플레이어 골인
-    GameEndCountDown : 25,      // 게임 종료 카운트 다운
-    GameEnd : 26,               // 게임 종료
-    GameSync : 27,              // position update
-    Break : 28,                 // 브레이크 입력 
+    LoadGameScene : 1,         // 인게임 씬으로 전환  - 멍청 레이싱에서는 playerconnect로 사용
+    PlayerReady : 2,           // 로딩 완료
+    GameStartCountDown : 3,         // 게임 시작 / 끝 카운트 다운
+    GameStart : 4,             // 게임 시작
+    PlayerDisconnect : 5,
+    PlayerGoal : 6,            // 플레이어 골인
+    GameEndCountDown : 7,      // 게임 종료 카운트 다운
+    GameEnd : 8,               // 게임 종료
     Sync : 30,                  // 동기화
-
 
     ResetServer : 100,          // 서버 리셋
 
